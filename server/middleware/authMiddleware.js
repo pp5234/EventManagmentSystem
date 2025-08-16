4c99206a143024e32bc151b90f42fda7dd744f02
pp5234 <ppetrovic10221ri@raf.rs>
2025-08-16 13:36:00 +0200

    feat: add authentication middleware

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import {ForbiddenError} from "../utils/errors.js";
+import {verifyJwt} from "../utils/jwt.js";
+
+const JsonWebTokenError = jwt.JsonWebTokenError
+
+export async function verifyToken(req, res, next) {
+    try {
+        const authHeader = req.headers.authorization;
+        if(!authHeader || !authHeader.startsWith('Bearer ')) {
+            return next(new JsonWebTokenError("No token provided"));
+        }
+        const token = authHeader.split(' ')[1];
+        if(!token) {
+            return next(new JsonWebTokenError("No token provided"));
+        }
+        req.user = verifyJwt(token);
+
+        next();
+    } catch (err) {
+        next(err)
+    }
+}
+
+export function isAdmin(req, res, next) {
+    if (req.user && req.user.type === 'ADMIN') {
+        return next();
+    }
+    return next(new ForbiddenError("Access denied. Admins only."));
+}
