2a482445a518a0258d4337d46562a1b4f7d5da16
pp5234 <ppetrovic10221ri@raf.rs>
2025-08-28 17:44:42 +0200

    refactor: Allow unlimited registrations when capacity is null
    Now, if capacity is null, registrations are unlimited.
    Additionally, prevent users from using an EMS client's email
    unless authenticated as that client

--- server/controllers/registrationController.js
+++ server/controllers/registrationController.js
@@ -1,12 +1,14 @@
 import {BadRequestError} from "../utils/errors.js";
 import {createRegistration} from "../services/registrationService.js";
+import {verifyTokenGraceful} from "../middleware/authMiddleware.js";
 
 export async function registrationController(req, res, next) {
     const id = req.params.id;
     const { email } = req.body;
     if (!email)
         return next(new BadRequestError(`No email provided for registration`));
-    const result = await createRegistration(id, email)
+    verifyTokenGraceful(req)
+    const result = await createRegistration(id, email, req.user)
     if(result.affectedRows === 0)
         return next(new BadRequestError(`Id for event not found`));
     return res.sendStatus(204);
--- server/services/registrationService.js
+++ server/services/registrationService.js
@@ -1,13 +1,21 @@
 import pool from "../config/db.js";
+import {ForbiddenError} from "../utils/errors.js";
 
-export async function createRegistration(id, email) {
+export async function createRegistration(id, email, user) {
+    const [sqlUser] = await pool.query("SELECT * FROM client WHERE email = ?", [email]);
+
+    if (sqlUser[0].email && sqlUser[0].email !== user.email)
+        throw new ForbiddenError("Only logged user with that email can register")
+
     const sql = `
         INSERT INTO registration (event_id, email)
         SELECT ?, ?
         FROM event
         WHERE event_id = ?
-          AND capacity IS NOT NULL
-          AND (SELECT COUNT(*) FROM registration WHERE event_id = ?) < capacity;
+          AND (
+            capacity IS NULL
+            OR (SELECT COUNT(*) FROM registration WHERE event_id = ?) < capacity
+            );
     `;
     const [result] = await pool.query(sql, [id, email, id, id]);
     return result;
